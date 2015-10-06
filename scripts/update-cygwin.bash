7a6222e5f12387f0ee7c43cda2fbaee5122c8639
Steve Lefevre <lefevre.10@osu.edu>
2015-10-05 21:20:10 -0400

    Made cygwin update unattended

--- scripts/update-cygwin.bash
+++ scripts/update-cygwin.bash
@@ -1,4 +1,4 @@
-cd ~/Downloads; rm setup-x86.exe; 
-wget https://www.cygwin.com/setup-x86.exe; 
-chmod 744 ~/Downloads/setup-x86.exe; 
-play ~/Downloads/setup-x86.exe & exit
+cd ~/Downloads; rm setup-x86.exe;
+wget https://www.cygwin.com/setup-x86.exe;
+chmod 744 ~/Downloads/setup-x86.exe;
+play ~/Downloads/setup-x86.exe --no-desktop --no-shortcuts --no-startmenu --quiet-mode
