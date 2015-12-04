e38740a662ce8396c9f66224d8fff6ee0b6ae3f1
Steve Lefevre <lefevre.10@osu.edu>
2015-12-03 23:17:44 -0500

    Divvied up cygwin update scripts for x86 and x86-64

---
+++ scripts/update-cygwin-x86-64.bash
@@ -0,0 +1,4 @@
+cd ~/Downloads; rm setup-x86_64.exe;
+wget https://www.cygwin.com/setup-x86_64.exe;
+chmod 744 ~/Downloads/setup-x86_64.exe;
+run ~/Downloads/setup-x86_64.exe --no-desktop --no-shortcuts --no-startmenu --quiet-mode
