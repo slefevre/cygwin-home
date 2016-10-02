98e53313e931fcd0598cadf413cfde4d3eb66e4e
Steve Lefevre <lefevre.10@osu.edu>
2016-10-01 22:44:33 -0400

    Building ssh keygen script

--- scripts/build-keys.bash
+++ scripts/build-keys.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo Please enter your new password:
-read pw1
-echo Please re-enter your new password:
-read pw2
+read -sp "Please enter your new password: " pw1
+echo
+read -sp "Please re-enter your new password: " pw2
 if [ "$pw1" != "$pw2" ]; then
+  echo
   echo Passwords do not match.
   exit 1
 fi
