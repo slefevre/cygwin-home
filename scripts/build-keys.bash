f60301d7a62f3224f51baf03d50028a39174480a
Steve Lefevre <lefevre.10@osu.edu>
2016-10-02 10:52:58 -0400

    Added line to actually create keys

--- scripts/build-keys.bash
+++ scripts/build-keys.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 read -p "Please enter your email: " email
+read -p "Please enter the host name of the target system: " system
 read -sp "Please enter your new password: " pw1
 echo
 read -sp "Please re-enter your new password: " pw2
@@ -15,5 +16,6 @@ hosts=($(cut -d' ' -f1 <~/.ssh/known_hosts|cut -d',' -f1|cut -d':' -f1|tr -d "[]
 for host in "${hosts[@]}"
 do
    echo "$host"
+   ssh-keygen -t ed25519 -C "$email" -P "$pw1" -f "$system_$email_$host.private.id_rsa"
    # or do whatever with individual element of the array
 done
